452363203c8a1a5442112c82ffc15503b2d941b7
Dan S. Myers <dmyers@rollins.edu>
2022-01-12 10:37:36 -0500

    Create variable_basics.py

---
+++ Examples/1-Variables/variable_basics.py
@@ -0,0 +1,23 @@
+"""
+This is a docstring. It's enclosed in triple double quotes.
+The docstring describes the purpose of your program: illustrate basic variable use.
+"""
+
+# A variable is a name associated with a value
+# **Values** in Python are basic data: numbers, strings
+
+# Mental model: a "box" with a name that holds some data
+# Refer to the data in the box using its name
+
+# First example: area of a rectangle
+
+# Create variables to describe the length and width of
+# the rectangle
+length = 3
+width = 5
+
+# Use a variable assignment to calculate the area
+area = length * width
+
+# You can print the value of a variable by printing its name
+print(area)
