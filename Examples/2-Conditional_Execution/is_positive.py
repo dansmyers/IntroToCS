0e2849ebc003961468f5fe53a687464018ecc675
Dan S. Myers <dmyers@rollins.edu>
2022-01-24 14:34:28 -0500

    Create is_positive.py

---
+++ Examples/2-Conditional_Execution/is_positive.py
@@ -0,0 +1,14 @@
+"""
+Test if an input number is positive, negative, or zero
+"""
+
+value = int(input('Type a number.'))
+
+if value > 0:
+    print('Positive.')
+elif value < 0:
+    print('Negative.'
+else:
+    print('Zero.')
+    
+print('Done.')
