44e52b09de657676382d3b8dd0ec09e46747ab56
Dan S. Myers <dmyers@rollins.edu>
2022-01-31 11:43:02 -0500

    Create green_eggs_and_ham.py

---
+++ Examples/3-Functions/green_eggs_and_ham.py
@@ -0,0 +1,28 @@
+"""
+Would you?
+CMS 120
+"""
+
+def print_refrain():
+    print()  # Blank line
+    print('I do not like them, Sam-I-Am.')
+    print('I do not like Green Eggs and Ham!')
+    print()  # Blank line
+    
+    
+### Main
+
+print('Would you like them in a house?')
+print('Would you like them with a mouse?')
+
+print_refrain()
+
+print('Would you eat them in a box?')
+print('Would you eat them with a fox?')
+
+print_refrain()
+
+print('Would you like them in Japan?')
+print('With Godzilla and Rodan?')
+
+print_refrain()
