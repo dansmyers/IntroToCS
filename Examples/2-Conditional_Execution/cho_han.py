afec53b18a3b75bacaa01d96c5b22ebca2038ed3
Dan S. Myers <dmyers@rollins.edu>
2024-09-16 11:56:58 -0400

    Update cho_han.py

--- Examples/2-Conditional_Execution/cho_han.py
+++ Examples/2-Conditional_Execution/cho_han.py
@@ -1,35 +1,43 @@
 """
-Cho-Han: roll two dice and bet whether the sum is even or odd
+CHO-HAN
 """
 
-from random import seed, randint
-import sys
+# Import the randint method
+from random import randint
 
-# Set the random number generator's seed
-# DON'T MODIFY THIS LINE
-seed(0)
-
-print('Welcome to Cho-Han.')
+# Print the welcome message and the two bets
+print('Welcome to cho-han.')
 print('1. Even')
 print('2. Odd')
 
-bet = int(input('Select a bet: '))
+# Read the number 1 or 2 from the user
+choice = int(input('Enter 1 or 2: '))
 
-if not (bet == 1 or bet == 2):
-    print('You must enter 1 or 2.')
-    sys.exit()
+# Check if the user's input is 1 or 2; if not, print an error and quit
+if choice < 1 or choice > 2:
+    print('That is not a valid choice.')
+    quit()
 
+
+# Roll two six-sided dice and add their sum
 die1 = randint(1, 6)
 die2 = randint(1, 6)
+print(f'The dice are {die1} and {die2}.')
 
-roll = die1 + die2
+total = die1 + die2
+print(f'The total is {total}.')
 
-print('The dice are %d and %d.' % (die1, die2))
-print('The sum is %d.' % roll)
-
-if roll % 2 == 0 and bet == 1:
-    print('You win.')
-elif roll % 2 == 1 and bet == 2:
-    print('You win.')
+# Use a conditional block to print the output
+#
+# There are two ways to win:
+#     If the player entered 1 and the sum is even
+#     If the player entered 2 and the sum is odd
+# Every other outcome is a loss
+#
+# Think about how to turn that into an if-elif-else block
+if choice == 1 and total % 2 == 0:
+    print('Win')
+elif choice == 2 and total % 2 != 0:
+    print('Win')
 else:
-    print('You lose.')
+    print('Lose')
