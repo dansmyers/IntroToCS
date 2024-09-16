9d21a8c91f525706feba88d2a818494af827c77b
Dan S. Myers <dmyers@rollins.edu>
2024-09-16 11:57:18 -0400

    Update cho_han.py

--- Examples/2-Conditional_Execution/cho_han.py
+++ Examples/2-Conditional_Execution/cho_han.py
@@ -18,7 +18,6 @@ if choice < 1 or choice > 2:
     print('That is not a valid choice.')
     quit()
 
-
 # Roll two six-sided dice and add their sum
 die1 = randint(1, 6)
 die2 = randint(1, 6)
@@ -33,8 +32,6 @@ print(f'The total is {total}.')
 #     If the player entered 1 and the sum is even
 #     If the player entered 2 and the sum is odd
 # Every other outcome is a loss
-#
-# Think about how to turn that into an if-elif-else block
 if choice == 1 and total % 2 == 0:
     print('Win')
 elif choice == 2 and total % 2 != 0:
